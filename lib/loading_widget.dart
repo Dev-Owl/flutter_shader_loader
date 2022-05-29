8059ba2f96b713b39490c369051ed35d866e2663
DevOwl <christian.muehle@devowl.de>
2022-05-29 17:04:26 +0200

    Small things

--- lib/loading_widget.dart
+++ lib/loading_widget.dart
@@ -13,6 +13,7 @@ class _ShaderSpinnerState extends State<ShaderSpinner> {
   Duration _previous = Duration.zero;
   late final Ticker _ticker;
   double dt = 0;
+
   @override
   void initState() {
     _ticker = Ticker(_tick);
@@ -20,9 +21,15 @@ class _ShaderSpinnerState extends State<ShaderSpinner> {
     super.initState();
   }
 
+  @override
+  void dispose() {
+    _ticker.stop();
+    _ticker.dispose();
+    super.dispose();
+  }
+
   void _tick(Duration timestamp) {
     final durationDelta = timestamp - _previous;
-
     _previous = timestamp;
     setState(() {
       dt += durationDelta.inMicroseconds / Duration.microsecondsPerSecond;
@@ -33,7 +40,7 @@ class _ShaderSpinnerState extends State<ShaderSpinner> {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return CustomPaint(
-      painter: ShaderPainter(dt),
+      painter: ShaderPainter(dt * 1.8),
       size: size,
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -5,21 +5,13 @@ import 'package:flutter_shader_loader/loading_widget.dart';
 late final Loading loadingShader;
 Future<void> main() async {
   loadingShader = await Loading.compile();
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  runApp(
+    MaterialApp(
       title: 'Fancy Loading',
       theme: ThemeData.dark(),
       home: const LoadingScreen(),
-    );
-  }
+    ),
+  );
 }
 
 class LoadingScreen extends StatelessWidget {
